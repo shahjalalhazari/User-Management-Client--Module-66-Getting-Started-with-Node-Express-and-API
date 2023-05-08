9f151cb9a4d2d26d42f78642b99833eb27344a1e
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2023-05-08 20:02:37 +0400

    Project Created.

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  });
+
+  const handleAddUser = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+    // Sending data from client side to server side via POST method.
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+        form.reset();
+      });
+  };
+
+  return (
+    <>
+      <h1>User Management System</h1>
+      <p>Number of Users: {users.length}</p>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+      <div>
+        {users.map((user) => (
+          <p key={user.id}>
+            {user.id} | {user.name} | {user.email}
+          </p>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
